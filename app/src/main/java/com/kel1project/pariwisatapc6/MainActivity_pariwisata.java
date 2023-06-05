70ed88d20fa2fd1172fe89ccafbe4c7d2066577e
Ethan Antonio <ethan@Ethans-MacBook-Air.local>
2023-06-05 19:28:39 +0700

    Initial commit

---
+++ app/src/main/java/com/kel1project/pariwisatapc6/MainActivity_pariwisata.java
@@ -0,0 +1,19 @@
+package com.kel1project.pariwisatapc6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.FragmentActivity;
+
+/*
+ * Main Activity class that loads {@link MainFragment}.
+ */
+public class MainActivity_pariwisata extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_pariwisata);
+    }
+}
